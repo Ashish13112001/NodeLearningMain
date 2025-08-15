701c644799dadc155739f567cb017604e81dd44c
Ashish13112001 <barthwal13nov@gmail.com>
2025-08-15 17:38:06 +0530

    section-10_V-145

--- app.js
+++ app.js
@@ -7,19 +7,24 @@ const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const app = express();
 
 console.log(process.env.NODE_ENV);
 
 // Global middleware
+
+// Set security HTTP headers
 app.use(helmet());
 
-
+// Development logging
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev')); 
 }
 
+// Limit request from same API
 const limiter = rateLimit({
     max: 3,
     windowMs: 60*60*1000,
@@ -27,11 +32,20 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter); 
 
-app.use(express.json()); // eski vajah s req.body console m show hora h(need to R&D)
+// Body parser, reading data from body into req.body
+app.use(express.json( { limit: '10kb'})); // eski vajah s req.body console m show hora h(need to R&D)
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against xss
+app.use(xss());
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
 
-
+// Random test middleware
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     next();
